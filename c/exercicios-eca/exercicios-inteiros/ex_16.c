4604dcc2f8e6c7c6cd0430d239f018c81d1ca56c
Jhonata Polito <jhonata.demuner@gmail.com>
2022-09-08 14:22:37 -0300

    Organizando pastas

---
+++ c/exercicios-eca/exercicios-inteiros/ex_16.c
@@ -0,0 +1,21 @@
+// Dado um número natural na base binária, transformá-lo para a base decimal.
+// Exemplo: Dado 10010 a saída será 18, pois 1. 2 4 + 0. 2 3 + 0. 2 2 + 1. 2 1 + 0. 2 0 = 18.
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int binario;
+    int expoente;
+    int multiplicacao;
+    char decimal[];
+    int i;
+    
+    while (decimal[i] != "\0"){
+        printf("%c", decimal[i]);
+        i++;
+    }
+
+    return 0;
+}
