e460f9d262c9db95fd3b6cfb9eb15e649b87e8cb
Jhonata Polito <jhonata.demuner@gmail.com>
2022-11-07 16:18:18 -0300

    Criando exercício NodeList

---
+++ javascript/curso-javascript/logica-programacao/nodelist/assets/js/main.js
@@ -0,0 +1,9 @@
+const paragrafos = document.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+for (let p of paragrafos){
+    p.style.backgroundColor = backgroundColorBody
+    p.style.color = 'White'
+}
